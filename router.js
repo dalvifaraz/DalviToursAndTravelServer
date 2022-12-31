bf21593d93556ec5cdccc4b816950a4bcdf8ad77
Faraz Dalvi <farazdalvi@Mangals-MacBook-Pro.local>
2022-12-31 23:59:55 +0530

    server files pushed

---
+++ router.js
@@ -0,0 +1,28 @@
+// router.js
+const { API_KEY, API_SECRET } = require("./config");
+const Amadeus = require("amadeus");
+const express = require("express");
+// Create router
+const router = express.Router();
+// Create Amadeus API client
+const amadeus = new Amadeus({
+  clientId: API_KEY,
+  clientSecret: API_SECRET,
+});
+
+router.get(`/search-location`, async (req, res) => {
+    try {
+      const { keyword, pageLimit, pageOffset } = req.query;
+      const response = await amadeus.referenceData.locations.get({
+        keyword,
+        'page[limit]': pageLimit,
+        'page[offset]': pageOffset,
+        subType: Amadeus.location.city,
+      });
+      res.json(JSON.parse(response.body));
+    } catch (err) {
+      res.json(err);
+    }
+  });
+
+  module.exports = router;
